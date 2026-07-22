383a0cff99ab4cea46e6d93a65dc4f84f0fe320c
daviderocco85 <roccodavide85@gmail.com>
2026-07-22 23:41:43 +0200

    feat: add movie list with genre filter and useEffect logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import './App.css';
 
@@ -13,9 +13,35 @@ const movies = [
 
 
 export const App = () => {
+  const [genreFilter, setGenreFilter] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+
+  useEffect(() => {
+    setFilteredMovies(
+      genreFilter === ""
+        ? movies
+        : movies.filter(movie => movie.genre === genreFilter)
+    );
+  }, [genreFilter]);
+
   return (
     <>
-      Works
+      <div>
+        <h1>La mia lista di Film</h1>
+        <ul>
+          {filteredMovies.map((movie, index) => (
+            <li key={index}>{movie.title}</li>
+          ))}
+        </ul>
+        <p>Scegli i film per genere</p>
+        <select value={genreFilter} onChange={(e) => setGenreFilter(e.target.value)}>
+          <option value="">Tutti i generi</option>
+          <option value="Azione">Azione</option>
+          <option value="Fantascienza">Fantascienza</option>
+          <option value="Romantico">Romantico</option>
+          <option value="Thriller">Thriller</option>
+        </select>
+      </div>
     </>
   )
 };
